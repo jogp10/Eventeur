896b6e2516c48565e0f4b4cad3536aa967599dd2
BernardoFerreira00 <56405746+BernardoFerreira00@users.noreply.github.com>
2022-11-05 13:30:05 +0000

    added hardcoded homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,94 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-
-  </div>
+<div class="container">
+        <div class="row border border-dark m-5">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-1 ps-5">
+                        <p>32</p>
+                    </div>
+                    <div class="col-8">
+                        <h3>Vegan for Beginners</h3>
+                        <p class="mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium tempora asperiores quia
+                            similique
+                            iste aliquid quam velit voluptas sint voluptate, aperiam nam fugit laudantium neque.</p>
+                        <div class="d-flex flex-row justify-content-between">
+                            <p>Comment</p>
+                            <p>Created by fjienf jifwe</p>
+                            <p>3:35 PM 27/02/2022</p>
+                        </div>
+                    </div>
+                    <div class="col-3 pe-0 me-0">
+                        <img src="images/community-events.jpeg" class="img-fluid m-0 p-0" height="300" width="400" alt="...">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-row row border border-dark m-5">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-1 ps-5">
+                        <p>32</p>
+                    </div>
+                    <div class="col-8">
+                        <h3>An introduction to essencial Tai Chi skills</h3>
+                        <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Quae reprehenderit cupiditate corporis. Recusandae, reprehenderit dolores!</p>
+                        <div class="d-flex flex-row justify-content-between">
+                            <p>Comment</p>
+                            <p>Created by fjienf jifwe</p>
+                            <p>3:35 PM 27/02/2022</p>
+                        </div>
+                    </div>
+                    <div class="col-3 pe-0 me-0">
+                        <img src="images/community-events.jpeg" class="img-fluid m-0 p-0" height="200" width="300" alt="...">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-row row border border-dark m-5">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-1 ps-5">
+                        <p>32</p>
+                    </div>
+                    <div class="col-8">
+                        <h3>The Art of Erasus: Drawing and Destruction.</h3>
+                        <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, ab.</p>
+                        <div class="d-flex flex-row justify-content-between">
+                            <p>Comment</p>
+                            <p>Created by fjienf jifwe</p>
+                            <p>3:35 PM 27/02/2022</p>
+                        </div>
+                    </div>
+                    <div class="col-3 pe-0 me-0">
+                        <img src="images/community-events.jpeg" class="img-fluid m-0 p-0" height="200" width="300" alt="...">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-row row border border-dark m-5">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-1 ps-5">
+                        <p>32</p>
+                    </div>
+                    <div class="col-8">
+                        <h3>Knee Pain Corrective Exercise Workshop</h3>
+                        <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, error rem eveniet velit dolorum, iusto veritatis molestias excepturi eum adipisci deleniti eaque laborum? Alias doloremque quos possimus voluptates corporis molestias blanditiis aperiam eveniet maxime, at non odit 
+                            eum ab dolore tempore exercitationem vero quam quisquam nam. Delectus laborum dolor sint?</p>
+                        <div class="d-flex flex-row justify-content-between">
+                            <p>Comment</p>
+                            <p>Created by fjienf jifwe</p>
+                            <p>3:35 PM 27/02/2022</p>
+                        </div>
+                    </div>
+                    <div class="col-3 p-0 m-0">
+                        <img src="images/community-events.jpeg" class="img-fluid m-0 p-0" height="200" width="300" alt="...">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
