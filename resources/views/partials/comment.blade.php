e8ed83cf109148f97aa96790fbd85cbc42a8ba83
jogp10 <jogp10@gmail.com>
2022-12-11 20:34:17 +0000

    answer fix

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -21,12 +21,14 @@
           <span>Edit</span>
         </button>
         @endif
+        @if (Auth::check() && (Auth::id() == $comment->user_id || (Auth::user()->user->tickets && Auth::user()->user->tickets->where('event_id', $comment->event_id)->first()) || (Auth::user()->user->events && Auth::user()->user->events->where('event_id', $comment->event_id)->first())))
         <button id="{{$comment->id}}" type="button" class="event_comment col btn btn-link" style="text-decoration: none;color: black;">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left" viewBox="0 0 16 16">
             <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
           </svg>
           <span>Reply</span>
         </button>
+        @endif
         <button type="button" class="col btn btn-link" style="text-decoration: none;flex-grow: 2;color: black;">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
             <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
