fab558f815cc9133dff5d0ee18d1a9687a90fce8
BernardoFerreira00 <56405746+BernardoFerreira00@users.noreply.github.com>
2022-12-20 16:05:21 +0000

    finished polls.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,7 @@
   <script src={{ asset('js/invite.js') }} defer></script>
   <script src={{ asset('js/editProfile.js') }} defer></script>
   <script src={{ asset('js/comment.js') }} defer></script>
+  <script src={{ asset('js/event.js') }} defer></script>
 </head>
 <title></title>
 <link rel="icon" href="{!! asset('/images/logo.png') !!}" />
@@ -52,8 +53,6 @@
     </section>
   </main>
   @include("partials.footer")
-
-
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
 </body>
